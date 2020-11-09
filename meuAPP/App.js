5fd73a713b6938c4e57b263fed403ade9d31ffa1
Felipe Andrade Gouvea Tintori <felipe.tintori@leomadeiras.com.br>
2020-11-09 10:44:51 -0300

    Entendendo as PROPS

--- meuAPP/App.js
+++ meuAPP/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 
 class App extends Component{
   render(){
+
+    let nome ='Felipe'
+    let img = 'https://sujeitoprogramador.com/steve.png'
+  
     return(
       <View>
         <Text>Olá Mundo</Text>
+        <Text style={{color : 'red', fontSize: 25, margin: 15}}>Sujeito Programador</Text>
+        <Image style={{width: 300, height: 300}} source={{uri: img}} />
+        <Text style={{fontSize: 30}}>{nome}</Text>
       </View>
-    )
+    ) 
   }
 }
 
