ad3de310e6d3b4d475a8884fcf7a1301b74f9643
Felipe Andrade Gouvea Tintori <felipe.tintori@leomadeiras.com.br>
2020-11-09 10:56:29 -0300

    Criando Componentes

--- meuAPP/App.js
+++ meuAPP/App.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react'
 import { View, Text, Image } from 'react-native'
 
-class App extends Component{
-  render(){
+class App extends Component {
+  render() {
 
-    let nome ='Felipe'
-    let img = 'https://sujeitoprogramador.com/steve.png'
-  
-    return(
+    let nome = 'Felipe'
+
+
+    return (
       <View>
         <Text>Olá Mundo</Text>
-        <Text style={{color : 'red', fontSize: 25, margin: 15}}>Sujeito Programador</Text>
-        <Image style={{width: 300, height: 300}} source={{uri: img}} />
-        <Text style={{fontSize: 30}}>{nome}</Text>
+        <Text style={{ color: 'red', fontSize: 25, margin: 15 }}>Sujeito Programador</Text>
+        
+        <Text style={{ fontSize: 30 }}>{nome}</Text>
+        <Jobs largura={500} altura={200} nome={'Steve Jobs'} />
       </View>
-    ) 
+    )
+  }
+}
+
+class Jobs extends Component {
+  render() {
+    let img = 'https://sujeitoprogramador.com/steve.png'
+    return (
+      <View>
+
+        <Image style={{ width: this.props.largura, height: this.props.altura }} source={{ uri: img }} />
+        <Text>{this.props.nome}</Text>
+      </View>
+    )
   }
 }
 
