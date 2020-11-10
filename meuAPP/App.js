9bbf3913115fad289909cb557f0ed532c260d435
Felipe Andrade Gouvea Tintori <felipe.tintori@leomadeiras.com.br>
2020-11-10 11:05:40 -0300

    Tamanhos Dinamicos e Fixos

--- meuAPP/App.js
+++ meuAPP/App.js
@@ -1,31 +1,19 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text} from 'react-native'
 
 class App extends Component {
   render() {
     return (
-      <View style={styles.area}>
-        <Text style={[styles.textoPrincipal, styles.alinhaTexto]}>Texto 1</Text>
-        <Text style={styles.alinhaTexto}>Texto 2</Text>
-        <Text>Texto 3</Text>
-        <Text style={styles.textoPrincipal}>Texto 4</Text>        
+      <View style={{flex: 1, backgroundColor: '#222'}}>
+       <View style={{flex:1, backgroundColor: 'red'}}></View>       
+       <View style={{flex:1,  backgroundColor: 'green'}}></View>       
+       <View style={{flex:2,  backgroundColor: 'yellow'}}></View>  
       </View>
     )
   }
 }
 
-const styles = StyleSheet.create({
-  area:{
-    marginTop: 40
-  },
-  textoPrincipal:{
-    fontSize: 25,
-    color: '#ff0000'
-  },
-  alinhaTexto:{
-    textAlign: 'center'
-  }
-});
+
 
 
 
