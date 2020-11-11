d3e02aa22793a50ab8b189ce059ff8adaadc65b3
Felipe Andrade Gouvea Tintori <felipe.tintori@leomadeiras.com.br>
2020-11-11 09:04:16 -0300

    FlexBox e Alinhamentos

--- meuAPP/App.js
+++ meuAPP/App.js
@@ -4,10 +4,11 @@ import { View, Text} from 'react-native'
 class App extends Component {
   render() {
     return (
-      <View style={{flex: 1, backgroundColor: '#222'}}>
-       <View style={{flex:1, backgroundColor: 'red'}}></View>       
-       <View style={{flex:1,  backgroundColor: 'green'}}></View>       
-       <View style={{flex:2,  backgroundColor: 'yellow'}}></View>  
+      <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
+       <View style={{width: 50, height: 50, backgroundColor: 'green'}}></View>       
+       <View style={{width:50,  height: 50, backgroundColor: 'red'}}></View>       
+       <View style={{width:50,  height: 50, backgroundColor: 'yellow'}}></View> 
+       <View style={{width:50,  height: 50, backgroundColor: 'blue'}}></View>  
       </View>
     )
   }
