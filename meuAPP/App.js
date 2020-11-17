6da749c08dd22133019d02dc092cc8659498e475
Felipe Andrade Gouvea Tintori <felipe.tintori@leomadeiras.com.br>
2020-11-17 11:23:33 -0300

    App Cronometro

--- meuAPP/App.js
+++ meuAPP/App.js
@@ -5,41 +5,69 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      textoFrase: '',
-      img: require('./android/app/src/biscoito-fechado.jpg')
+    this.state = {     
+      img: require('./android/app/src/cronometro.png'),
+      numero: 0,
+      botao: "VAI",
+      ultimo: null
 
     }
 
-    this.quebraBiscoito = this.quebraBiscoito.bind(this)
+    //Variavel do timer relogio
+    this.timer = null;
 
-    this.frases = [
-      'Siga os bons e aprenda com eles',
-      'O bom-senso vale mais do que muito conhecimento',
-      'O riso é a menor ditância entre duas pessoas',
-      'Deixe de lado as preocupações e seja feliz'
-    ];
+    this.vai = this.vai.bind(this)
+    this.limpar = this.limpar.bind(this)
+
+   
+
+   
   }
 
-  quebraBiscoito() {
-    let numeroAleatorio = Math.floor(Math.random() * this.frases.length)
-    this.setState({
-      textoFrase: this.frases[numeroAleatorio]
-    })
+  vai(){
+
+    if(this.timer != null){
+      clearInterval(this.timer)
+      this.timer = null
+      this.setState({botao: "VAI"})
+    }else{
+      this.timer = setInterval(()=>{
+        this.setState({numero: this.state.numero + 0.1})
+        this.setState({botao: "PARAR"})
+      }, 100)
+    }
+  }
+
+  limpar(){
+    if(this.timer != null){
+      clearInterval(this.timer);
+      this.timer = null
+    }
+
+    
+    this.setState({numero:0, botao: "VAI", ultimo: this.state.numero})
+  
 
   }
+
 
   render() {
     return (
       <View style={styles.container}>
-        <Image style={styles.img} source={this.state.img} />
-        <Text style={styles.textoFrase}>"{this.state.textoFrase}"</Text>
-
-        <TouchableOpacity style={styles.botao} onPress={this.quebraBiscoito}>
-          <View style={styles.btnArea}>
-            <Text style={styles.btnTexto}>Quebrar Biscoito</Text>
-          </View>
-        </TouchableOpacity>
+        <Image style={styles.cronometro} source={this.state.img} />
+        <Text style={styles.timer}>{this.state.numero.toFixed(1)}</Text>
+        <View style={styles.btnArea}>
+          <TouchableOpacity style={styles.btn} onPress={this.vai}>
+            <Text style={styles.btnTexto}>{this.state.botao}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btn} onPress={this.limpar}>
+            <Text style={styles.btnTexto}>Limpar</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.areaUltimo}>
+    <Text style={styles.textoCorrida}>{this.state.ultimo > 0 ? this.state.ultimo.toFixed(2) + 's' : ''}</Text>
+        </View>
+       
       </View>
     )
   }
@@ -49,36 +77,45 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
-  },
-  img: {
-    width: 250,
-    height: 250,
-  },
-  textoFrase: {
-    fontSize: 20,
-    color: '#dd7b22',
-    margin: 30,
-    fontStyle: 'italic',
-    textAlign: 'center'
-  },
-  botao: {
-    width: 230,
-    height: 50,
-    borderWidth: 2,
-    borderColor: '#dd7b22',
-    borderRadius: 25
-  },
-  btnArea: {
-    flex: 1,
-    flexDirection: 'row',
     justifyContent: 'center',
-    alignItems: 'center'
+    backgroundColor: '#00aeef'
   },
-  btnTexto: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: '#dd7b22'
+  cronometro: {
+   
+  },
+  timer:{
+    marginTop: -160,
+    color: '#FFF',
+    fontSize: 65,
+    fontWeight: 'bold'
+  },
+  btnArea:{
+    flexDirection: 'row',
+    marginTop: 70,
+    height: 40
+  },
+  btn:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center', 
+    backgroundColor: '#FFF',
+    height: 40,
+    margin: 17,
+    borderRadius: 9
+  } ,
+  btnTexto:{
+    fontSize:20,
+    fontWeight:'bold',
+    color: '#00aeef'
+  },
+  areaUltimo:{
+    marginTop: 40
+
+  },
+  textoCorrida:{
+    fontSize: 25,
+    fontStyle: 'italic',
+    color: '#FFF'
   }
 
 });
