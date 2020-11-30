6dea29a4c96a8061ef149963c8a3e5fa69e4fae5
Felipe Andrade Gouvea Tintori <felipe.tintori@leomadeiras.com.br>
2020-11-30 09:28:47 -0300

    Instagram pt/1

--- meuAPP/App.js
+++ meuAPP/App.js
@@ -1,14 +1,60 @@
 import React, { Component } from 'react'
 
-import { View, StyleSheet, Text, Switch } from 'react-native'
-
+import { View, StyleSheet, Text, Image, TouchableOpacity, FlatList } from 'react-native'
+import Lista from './android/app/src/Lista'
 
 class App extends Component {
 
   constructor(props){
     super(props);
     this.state = {
-      status: false
+      feed: [
+        {
+          id: '1', 
+          nome: 'Lucas Silva', 
+          descricao: 'Mais um dia de muitos bugs :)', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil1.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto1.png',  
+          likeada: false, 
+          likers: 0
+         },
+        {
+          id: '2', 
+          nome: 'Matheus', 
+          descricao: 'Isso sim é ser raiz!!!!!', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil2.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto2.png', 
+          likeada: false, 
+          likers: 0
+        },
+        {
+          id: '3', 
+          nome: 'Jose Augusto', 
+          descricao: 'Bora trabalhar Haha', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil3.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto3.png',  
+          likeada: false, 
+          likers: 3
+        },
+        {
+          id: '4', 
+          nome: 'Gustavo Henrique', 
+          descricao: 'Isso sim que é TI!', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil1.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto4.png', 
+          likeada: false, 
+          likers: 1
+        },
+        {
+          id: '5', 
+          nome: 'Guilherme',  
+          descricao: 'Boa tarde galera do insta...', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil2.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto5.png',
+          likeada: false, 
+          likers: 32
+        }
+      ]
       
     }
   }
@@ -18,16 +64,18 @@ class App extends Component {
    
     return (
       <View style={styles.container}>
-        
-        <Switch
-        value={this.state.status}
-        onValueChange={(valorSwitch) => this.setState({status: valorSwitch})}
-        thumbColor="#FF0000"
-        />
-
-        <Text style={{textAlign: 'center', fontSize: 30}}>
-          {(this.state.status) ? "Ativo" : "Inativo"}
-        </Text>
+        <View style={styles.header}>
+          <TouchableOpacity>
+            <Image style={styles.logo} source={require('./android/app/src/img/logo.png')}/>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image style={styles.send} source={require('./android/app/src/img/send.png')}/>
+          </TouchableOpacity>
+        </View>
+        <View>
+        <FlatList showsHorizontalScrollIndicator={false} data={this.state.feed} renderItem={({item}) => <Lista data={item} /> }/>
+        </View>
+       
       </View>
     )
   }
@@ -36,8 +84,25 @@ class App extends Component {
 const styles = StyleSheet.create({
   container:{
     flex:1,
-    marginTop: 20
   },
+  header:{
+    height: 55,
+    backgroundColor: "#FFF",
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 5,
+    borderBottomWidth: 0.2,
+    shadowColor: '#000',
+    elevation:1
+  },
+  logo:{
+
+  },
+  send:{
+    width: 23,
+    height: 23
+  }
   
 })
 
