15002b9c76991828e1044bbf13dd98550163a49a
felipetintori <felipe_tintori@hotmail.com>
2020-12-17 09:11:43 -0300

    Conversor de moedas pt2

--- meuAPP/src/Conversor/index.js
+++ meuAPP/src/Conversor/index.js
@@ -2,29 +2,43 @@ import React, {Component} from 'react'
 
 import {View, Text, StyleSheet, TextInput, TouchableOpacity} from 'react-native'
 
-// https://free.currconv.com/api/v7/convert?q=USD_BRL&compact=ultra&apiKey=a865718d6b6cf3059f4e
+// convert?q=USD_BRL&compact=ultra&apiKey=a865718d6b6cf3059f4e
 
 class Conversor extends Component{
     constructor(props){
         super(props);
         this.state = {
-
+            moedaA: props.moedaA,
+            moedaB: props.moedaB,
+            moedaB_valor: 0,
+            valorConvertido: 0
         }
+        this.converter = this.converter.bind(this)
+    }
+
+    converter(){
+        
     }
     render(){
+        const { moedaA, moedaB}= this.props;
         return(
             <View style={styles.container}>
-                <Text style={styles.titulo}>USD para BRL</Text>
+                <Text style={styles.titulo}>{moedaA} para {moedaB}</Text>
 
                 <TextInput
                 placeholder="Valor a ser convertido"
                 style={styles.areaInput}
-                onChangeText={() => {}}
+                onChangeText={(moedaB_valor) => this.setState({moedaB_valor})}
+                keyboardType="numeric"
                 />
 
-                <TouchableOpacity style={styles.botaoArea}>
+                <TouchableOpacity style={styles.botaoArea} onPress={this.converter}>
                     <Text style={styles.botaoTexto}>Converter</Text>
                 </TouchableOpacity>
+
+                <Text style={styles.valorConvertido}>
+                    {this.state.valorConvertido}
+                </Text>
             </View>
         )
     }
@@ -32,8 +46,45 @@ class Conversor extends Component{
 
 const styles = StyleSheet.create({
    container:{
-
-   } 
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center'
+   },
+   titulo:{
+       fontSize: 30,
+       fontWeight: 'bold',
+       color: "#000"
+   },
+   areaInput:{
+       width: 280,
+       height: 45,
+       backgroundColor: '#CCC',
+       textAlign: 'center',
+       marginTop: 15,
+       fontSize: 20,
+       color: '#000',
+       borderRadius: 5
+   },
+   botaoArea:{
+       width: 150, 
+       height: 45,
+       backgroundColor: '#FF0000',
+       borderRadius: 5, 
+       justifyContent: 'center',
+       alignItems: 'center',
+       marginTop: 15,
+   },
+   botaoTexto:{
+       fontSize: 17,
+       fontWeight: 'bold',
+       color: '#FFF'
+   },
+   valorConvertido:{
+       fontSize: 30,
+       fontWeight: 'bold',
+       color: '#000',
+       marginTop: 15,
+   }
 })
 
 export default Conversor
